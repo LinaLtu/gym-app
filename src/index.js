ebfea7fe9f115a5b897668360e303ff9630dd86d
Lina <lsuodyte@gmail.com>
2020-07-26 16:07:23 +0200

    feat: add react router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
+import { createBrowserHistory } from 'history';
+import MainPage from './components/MainPage';
+import DetailsPage from './components/DetailsPage';
 import './index.css';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const history = createBrowserHistory();
+
+const router = (
+  <Router history={createBrowserHistory}>
+    <div>
+      <Route exact path="/" component={MainPage} />
+      <Route path="/course" component={DetailsPage} />
+    </div>
+  </Router>
 );
-
+ReactDOM.render(router, document.getElementById('root'));
