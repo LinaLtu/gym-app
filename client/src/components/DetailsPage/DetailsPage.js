322f62757b90e79d5463eb2bdfb3663203127af7
Lina <lsuodyte@gmail.com>
2020-07-27 16:20:11 +0200

    feat: add 'Go Back' button to DetailsPage

--- client/src/components/DetailsPage/DetailsPage.js
+++ client/src/components/DetailsPage/DetailsPage.js
@@ -1,4 +1,5 @@
 import React, { useState, Fragment, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../Header';
 import './DetailsPage.scss';
 
@@ -7,6 +8,12 @@ const CSS_NAME = 'details-page';
 
 const DetailsPage = () => {
 
+    const history = useHistory();
+
+    const handleOnClick = () => {
+        history.push("/");
+    }
+
     return (
         <div className={CSS_NAME}>
             <Header />
@@ -14,9 +21,9 @@ const DetailsPage = () => {
                 <div className={`${CSS_NAME}_course-name`}>"Course"</div>
                 <div className={`${CSS_NAME}_details`}>StartDate | Category</div>
                 <div className={`${CSS_NAME}_description`}>Description: Very very long</div>
-                <button className={`${CSS_NAME}_back-button`} >Go Back</button>
+                <button className={`${CSS_NAME}_back-button`} onClick={handleOnClick} >Go Back</button>
             </div>
-        </div>
+        </div >
     )
 
 }
