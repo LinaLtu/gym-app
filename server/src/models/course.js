861ff7c108b45c349b8167ea0b13ecf2ce75791d
Lina <lsuodyte@gmail.com>
2020-07-29 14:00:55 +0200

    fix: fix misspelled property name

--- server/src/models/course.js
+++ server/src/models/course.js
@@ -1,9 +1,9 @@
 module.exports = class Course {
-    constructor({ _id, name, description, startDate, categorty }) {
-        this.id = _id;
+    constructor({ _id, name, description, startDate, category }) {
+        this._id = _id;
         this.name = name;
         this.description = description;
         this.startDate = startDate;
-        this.categorty = categorty;
+        this.category = category;
     }
 }
